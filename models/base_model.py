d0b346aed56459b53f58aa5c481a0abb3f788aea
Ehmkaay <mk.agbola@gmail.com>
2023-12-10 14:46:48 -0800

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -19,14 +19,10 @@ class BaseModel():
                             setattr(self, k, datetime.strptime(v, a))
                         except:
                             setattr(self, k, datetime.now())
-<<<<<<< HEAD
                     else:
                         setattr(self, k, v)
-=======
                 else:
                     setattr(self, k, v)
->>>>>>> 9ed3aa9ddab8f419d3417dc287c39d1e0657756e
-
         else:
             """Initialize the class when an instance is created."""
             self.id = str(uuid4())
