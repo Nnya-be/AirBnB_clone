8b950567ed773cbe091f5f3f3d1d4067219b17c8
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2023-12-11 01:01:55 +0000

    Modified:

--- models/base_model.py
+++ models/base_model.py
@@ -10,16 +10,12 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initialize the class."""
         if kwargs:
-            for k, v in kwargs.items():
-                if k != '__class__':
-                    if k in ['created_at', 'updated_at']:
-                        try:
-                            date_format = '%Y-%m-%dT%H:%M:%S.%f'
-                            setattr(self, k, datetime.strptime(v, date_format))
-                        except ValueError:
-                            setattr(self, k, datetime.now())
-                    else:
-                        setattr(self, k, v)
+            d_f = '%Y-%m-%dT%H:%M:%S.%f'
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],d_f)
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],d_f)
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
+            
         else:
             from models import storage
             self.id = str(uuid.uuid4())
