b848fc9db7b495e72bcb02437857c1b3fd54ec72
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2023-12-12 10:29:01 +0000

    Added: user tests

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Tests for the user model"""
+from tests.test_models.test_base_model import TestBaseModel
+from models.user import User
+
+
+class test_User(TestBaseModel):
+    """ """
+
+    def __init__(self, *args, **kwargs):
+        """Test the init method."""
+        super().__init__(*args, **kwargs)
+        self.name = "User"
+        self.value = User
+
+    def test_first_name(self):
+        """Test for the first name."""
+        new = self.value()
+        self.assertEqual(type(new.first_name), str)
+
+    def test_last_name(self):
+        """Test for the last name."""
+        new = self.value()
+        self.assertEqual(type(new.last_name), str)
+
+    def test_email(self):
+        """Test for the email."""
+        new = self.value()
+        self.assertEqual(type(new.email), str)
+
+    def test_password(self):
+        """Test for the password"""
+        new = self.value()
+        self.assertEqual(type(new.password), str)
